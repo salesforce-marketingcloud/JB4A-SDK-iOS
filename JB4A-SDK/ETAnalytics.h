9d8a18b1581ecdd67306ab868222b317ee3a9e99
Tom Brooks <tbntb@sbcglobal.net>
2016-02-01 11:07:08 -0500

    added changes for 4.1.0
    added changes for 4.1.0

--- JB4A-SDK/ETAnalytics.h
+++ JB4A-SDK/ETAnalytics.h
@@ -11,7 +11,12 @@
 @interface ETAnalytics : NSObject
 
 /**
- Set PIEvent (WAMA) Web Analytics Mobile Analytics
+ Set Web Analytics Mobile Analytics, Track page views within your application.
+ @param url - a non-nil NSString to identify the location within your app traversed by your customers.  For example: com.yourpackage.viewcontrollername
+ @param title - a NSString (nil if n/a) to identify the title of the location within your app traversed by your customers. For example: Screen Name
+ @param item - a NSString (nil if n/a) to identify an item viewed by your customer.  For example: UPC-1234
+ @param search - a NSString (nil if n/a) to identify search terms used by your customer.  For example: blue jeans.
+ @return void
  */
 +(void)trackPageView:(NSString*)url andTitle:(NSString*)title andItem:(NSString *)item andSearch:(NSString*)search;
 
